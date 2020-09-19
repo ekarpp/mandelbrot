2cc233a8fd48b4228d57fe92c4386e6c78f125b4
ekarpp <eetu.karppinen@aalto.fi>
2020-09-19 18:19:28 +0300

    fix bug of iteration limit changing incorrectly

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ function init()
 
   render();
 
-  niters.onchange = () => {worker_data.lim = niters.value; render();}
+  niters.onchange = () => {worker_data.lim = Number(niters.value); render();}
   cfunc.onchange = () => {color_fun = cf_map[cfunc.value]; render();}
   threads.onchange = () => init_workers(Number(threads.value));
   points.onchange = point_jump;
